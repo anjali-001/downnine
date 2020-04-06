2939491e1c86fef5a5660f82b6f861b4344db681
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-04-06 18:08:30 +0530

    Updated Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,56 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react';
+import {ButtonContainer} from './Button';
+import {DataContext} from '../context';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom'
 
-export class Home extends Component {
-    render() {
-        return (
-            <div>
-                hello from home
-            </div>
-        )
-    }
+const Home = () => {
+    const {generateTitle,randomTitle} = useContext(DataContext);
+    
+    return ( 
+        <HomeGameWrapper className="container col-lg-6 col-sm-10 text-center mx-auto" >
+                <h4>Having trouble deciding your drink? Let your fate choose for you.</h4>
+                <p>Click below to see what's your cocktail.</p>
+                <div>
+                <ButtonContainer onClick={()=>generateTitle()}>click</ButtonContainer>
+                </div>
+                <Link to='/details'>
+                <h3>{randomTitle}</h3>
+                </Link>
+                
+                
+
+                
+            </HomeGameWrapper>
+     );
+}
+ 
+export default Home;
+
+const HomeGameWrapper = styled.div`
+{
+    padding: 20px;
+    background: var(--mainGrey);
+    color: var(--mainWhite);
+    border: 3px solid var(--lightYellow);
+    margin-top: 100px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.7), 0 6px 20px 0 rgba(0, 0, 0, 0.20);
+}
+p{
+    text-align:center;
 }
 
-export default Home
+h3{
+    color:var(--lightYellow);
+    text-align:center;
+    text-transform:uppercase;
+    
+}
+h3:hover, Link:hover{
+    text-decoration:none !important;
+}
+div{
+    text-align:center;
+}
+
+`
