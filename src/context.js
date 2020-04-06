18b20a95d75b5f1e59d37ef7fc09d1caa32b54d0
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-04-06 18:07:31 +0530

    Updated Context.js

--- src/context.js
+++ src/context.js
@@ -108,3 +108,69 @@ class MenuContextProvider extends Component {
 }
 
 export default MenuContextProvider
+
+
+
+
+
+// import React, {Component, createContext} from 'react';
+// import {menuItems, detailProduct} from './data';
+
+
+// export const DataContext = createContext();
+
+// class DataContextProvider extends Component {
+//     state = {
+//         products:[...menuItems],
+//         detailProduct: detailProduct,
+//         num:null,
+//         randomTitle:null
+//     }
+
+
+// showItems =(id)=> {
+//     const item = this.state.products.find(item=>item.id===id)
+//     return item;
+// }
+
+// handleDetail = (id) => {
+//     const item = this.showItems(id);
+//     this.setState(()=>{
+//         return {
+//             detailProduct:item
+//         }
+//     })
+// }
+// generateNum = () => {
+//     const num = Math.floor(Math.random()*9)+1;
+//     // this.setState(()=>{
+//     //     return{
+//     //         num:num
+//     //     }
+//     // })
+//     return num
+// }
+
+
+// generateTitle = () => {
+    
+//     const number = this.generateNum();
+//     const item = this.showItems(number)
+//     this.setState(()=>{
+//         return{
+//             randomTitle:item.title
+//         }
+//     })
+// } 
+
+
+// render(){
+//     return(
+//         <DataContext.Provider value = {{...this.state, handleDetail:this.handleDetail, generateNum:this.generateNum, generateTitle:this.generateTitle}}>
+//             {this.props.children}
+//         </DataContext.Provider>
+//     )
+// }
+// }
+
+// export default DataContextProvider;
