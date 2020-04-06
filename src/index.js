d952867e42d45695d156dba60c9041c4b41d6c2d
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-04-06 18:28:38 +0530

    Updated Index.js

--- src/index.js
+++ src/index.js
@@ -20,3 +20,28 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+// import {BrowserRouter as Route} from 'react-router-dom'
+// import DataContextProvider from './context';
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <DataContextProvider>
+//     <Route>
+//     <App />
+//     </Route>
+//     </DataContextProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
